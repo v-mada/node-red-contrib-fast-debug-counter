b589829fb208b645043a436a799b22772881e0c0
v-mada <95949731+v-mada@users.noreply.github.com>
2023-02-12 13:44:21 -0500

    fix missing registration rename

--- counterful.js
+++ counterful.js
@@ -218,7 +218,7 @@ module.exports = function(RED) {
         })
     }
 
-    RED.nodes.registerType("counter",DebugNode, {
+    RED.nodes.registerType("counterful",DebugNode, {
         settings: {
             debugUseColors: {
                 value: false,
